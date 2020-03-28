779b39fcc08725275feee85a49f1653944937555
Christian Edward Gruber <gruber@squareup.com>
2020-03-28 00:57:05 -0700

    Don't install the certs.

--- buildkite/agents/linux/setup.sh
+++ buildkite/agents/linux/setup.sh
@@ -79,7 +79,6 @@ wget --progress=dot:giga https://github.com/holgerbrandl/kscript/releases/downlo
 unzip kscript-${KSCRIPT_VERSION}-bin.zip
 sudo install kscript-${KSCRIPT_VERSION}/bin/* /usr/local/bin/
 
-install_package ca-certificates-java
 install_package openjdk-8-jdk-headless
 
 install_package build-essential
