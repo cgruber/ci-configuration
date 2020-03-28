84d135b65c2c6964ca563310e39cc17f68fdca16
Christian Edward Gruber <gruber@squareup.com>
2020-03-27 21:37:29 -0700

    Revert to clang 6.0

--- buildkite/agents/linux/setup.sh
+++ buildkite/agents/linux/setup.sh
@@ -76,8 +76,8 @@ wget --progress=dot:giga https://github.com/holgerbrandl/kscript/releases/downlo
 unzip kscript-${KSCRIPT_VERSION}-bin.zip
 sudo install kscript-${KSCRIPT_VERSION}/bin/* /usr/local/bin/
 
-install_package clang-10
-export CC=clang-10
+install_package clang-6.0
+export CC=clang-6.0
 
 install_package ca-certificates-java
 install_package openjdk-8-jdk-headless
